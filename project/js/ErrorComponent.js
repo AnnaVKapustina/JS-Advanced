25e9ddeea4b8fe21f9272973410a5167750c59fb
AnnaVKapustina <esaulkovaav@mail.ru>
2021-06-13 12:33:22 +0300

    Homework-6

---
+++ project/js/ErrorComponent.js
@@ -0,0 +1,5 @@
+Vue.component ('error', {
+    template: `<div>
+                <p class="error container">Не удается выполнить запрос к серверу</p>
+                </div>` 
+})
---
+++ project/js/FilterComponent.js
@@ -0,0 +1,8 @@
+Vue.component ('filter-comp', {
+    template: `<div>
+                <form action="#" class="search-form" @submit.prevent="$root.filter(userSearch)">
+                    <input type="text" class="search-field" v-model="userSearch">
+                    <button type="submit" class="btn-search"><i class="fas fa-search"></i></button>
+                </form>
+                </div>` 
+})
